004145f304a142d3552de78b3fd877458ecb37fd
Richard O'Dwyer <richard@richard.do>
2017-08-10 13:25:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+import os
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='parse-accept-language',
+    version='0.1.0.dev0',
+    packages=find_packages('src', exclude=('tests',)),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    description='Parse Accept-Language HTTP header',
+    author='Richard O\'Dwyer',
+    author_email='richard@richard.do',
+    maintainer='Chatbot Developers',
+    keywords='python, accept-language, parse, headers',
+    maintainer_email='chatbot-developers@babylonhealth.com',
+    license='Apache License 2.0',
+    long_description=(
+        'https://github.com/Babylonpartners/parse-accept-language'
+    ),
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
