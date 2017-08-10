02c109ac63275c6b5fdd1bcd718198b7788fa31e
Richard O'Dwyer <richard@richard.do>
2017-08-10 14:08:59 +0100

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='parse-accept-language',
-    version='0.1.0.dev0',
+    version='0.1.0',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
